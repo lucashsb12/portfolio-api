ebda015da7c16ada8f54fbd254d9444332a61531
Lucas Henrique <lucashsb12@gmail.com>
2022-01-09 21:01:04 -0300

    req api

--- estudo/http/index.js
+++ estudo/http/index.js
@@ -2,8 +2,12 @@ const http = require('http');
 const port = 3000
 const server = http.createServer((req, res) =>{
     res.statusCode = 200; // sinaliza que a requisição foi feita com sucesso
-    res.setHeader('Content-Type', 'text/html'); // seta o header para retornar html
-    res.end("<h1>Ola mundo!</h1>")
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    res.end(JSON.stringify({message: "Parabens! Voce se conectou ao servidor node!"}));
+    console.log(req, res);
 });
 
 server.listen(port, () =>{
